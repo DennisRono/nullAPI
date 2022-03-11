ec78d182a8b7ae825d792a8aafef94370bebd13a
DennisRono <finnneron1@gmail.com>
2022-03-11 08:05:01 +0300

    enough disease sugar shout courage

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+require('dotenv').config();
+const { registerDataSchema, loginDataSchema, regDataSchema, logDataSchema } = require("../schemas/user");
+
+//register a new user in the system
+router.post("/register", (req, res, next) => {
+
+})
 
 module.exports = router;
