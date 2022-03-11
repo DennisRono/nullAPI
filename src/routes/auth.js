69193c41eaf3a517ca9ec01c027b95d37bb4596a
DennisRono <finnneron1@gmail.com>
2022-03-11 08:40:00 +0300

    word cow coffee watch love

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const db = require("../includes/auth");
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
