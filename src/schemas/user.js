a05257efc2ff54fe159ad620c9b7bef57ef19fbc
DennisRono <finnneron1@gmail.com>
2022-03-11 08:33:01 +0300

    running interior carefully drew income

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -2,7 +2,7 @@ const Joi = require('@hapi/joi');
 
 const registerDataSchema = Joi.object({
     email: Joi.string().email().lowercase().required(),
-    phone: Joi.string().min(10).max(10).required(),
+    phone: Joi.string().min(10).max(10),
     password: Joi.string().min(8).required().strict(),
     cpassword: Joi.string().valid(Joi.ref('password')).required().strict()
 });
