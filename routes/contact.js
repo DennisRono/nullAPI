7fe8279b00f2b87db0389bb2d90ec3dbd1710086
DennisRono <finnneron1@gmail.com>
2022-10-07 21:10:00 +0300

    essential done for few almost

--- routes/contact.js
+++ routes/contact.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const db = require("../includes/contact")
 const { contactDataSchema } = require("../schemas/contact")
+const { sendEmail } = require("../includes/mailer")
 
 router.post('/contact', async (req, res) => {
     try {
@@ -11,6 +12,7 @@ router.post('/contact', async (req, res) => {
             let mobj = { name: validate.name, email: validate.email, phone: validate.phone, website: validate.website, brief: validate.brief, assets: validate.assets, messageid: messageid }
             db.create(mobj, (message, r) => {
                 //send mail to denniskibet
+                sendEmail("Testing", "testing local mailer", "bennkaiser1@gmail.com", "denniski@denniskibet.com", "admin@denniskibet.com")
                 res.json({status: 200, type: "success", message: message, response: r})
             })
         } catch (err) { res.json({"status": 500,"type":"Error","details":err}) }
