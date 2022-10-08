51e333fdee607db8b34a746a7db6f30121dba521
DennisRono <finnneron1@gmail.com>
2022-10-08 17:30:01 +0300

    quietly must road fed may

--- includes/contact.js
+++ includes/contact.js
@@ -7,8 +7,8 @@ let blogodb = {}
 blogodb.create = (data, callback) => {
     return new Promise((resolve, reject)=>{
         conn.query('INSERT INTO contact(Name, Email, Phone, Website, Brief, Assets, MessageID) VALUES(?,?,?,?,?,?,?)', [ data.name, data.email, data.phone, data.website, data.brief, data.assets, data.messageid ], (err, results)=>{
-            if(err){return callback('error adding data to the database', err)}
-            callback('successfully added data to the database', results)
+            if(err){return callback('err', 'error adding data to the database', err)}
+            callback('succ','successfully added data to the database', results)
         })
     })
 }
--- routes/contact.js
+++ routes/contact.js
@@ -25,14 +25,18 @@ router.post('/contact', upload.array('assets', 10), async (req, res) => {
         for (let i = 0; i < req.files.length; i++) {
             filenames = filenames+"#"+req.files[i].filename           
         }
-        console.log(filenames);
         let messageid = (new Date()).getTime().toString(36) + Math.random().toString(36).slice(2)
         try {
             let mobj = { name: validate.name, email: validate.email, phone: validate.phone, website: validate.website, brief: validate.brief, assets: filenames, messageid: messageid }
-            db.create(mobj, (message, r) => {
-                //send mail to denniskibet
-                //sendEmail("New Contact From Portfolio!", "Message from "+validate.name+"<br><br>"+validate.website+"<br><br>"+validate.phone+"<br><br>"+validate.brief, "dennisrkibet@gmail.com", validate.email, validate.email)
-                res.json({status: 200, type: "success", message: message, response: r})
+            console.log(mobj)
+            db.create(mobj, (status, message, r) => {
+                if(status==='err'){
+                    res.json({status: 500, type: "Error", message: message, response: r})
+                } else {
+                    //send mail to denniskibet
+                    sendEmail("New Contact From Portfolio!", "Message from "+validate.name+"<br><br>"+validate.website+"<br><br>"+validate.phone+"<br><br>"+validate.brief, "dennisrkibet@gmail.com", validate.email, validate.email)
+                    res.json({status: 200, type: "success", message: message, response: r})
+                }
             })
         } catch (err) { res.json({"status": 500,"type":"Error","details":err}) }
     } catch (err) {
