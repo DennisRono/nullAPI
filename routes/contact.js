8ca931ed158f1ec25d0dbe4e64477f08a9b827ee
DennisRono <finnneron1@gmail.com>
2022-10-08 14:50:00 +0300

    center let guard means pink

--- routes/contact.js
+++ routes/contact.js
@@ -8,67 +8,16 @@ const multer = require('multer')
 //image upload dest
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, path.join(__dirname, './uploads'));
+      cb(null, path.join(__dirname, './uploads1'));
     },
     filename: function (req, file, cb) {
-      cb(
-        null,
-        file.fieldname + '-' + Date.now() + file.originalname.match(/\..*$/)[0],
-      );
-    },
-  });
-  const multi_upload = multer({
-    storage,
-    fileFilter: (req, file, cb) => {
-      if (
-        file.mimetype == 'image/png' ||
-        file.mimetype == 'image/jpeg' ||
-        file.mimetype == 'image/jpg'
-      ) {
-        cb(null, true);
-      } else {
-        cb(null, false);
-        const err = new Error('Only .jpg .jpeg .png images are supported!');
-        err.name = 'ExtensionError';
-        return cb(err);
-      }
-    },
-  }).array('uploadImages', 10);
-  app.post('/api/upload', (req, res) => {
-      multi_upload(req, res, function (err) {
-        console.log(req.files);
-      //multer error
-      if (err instanceof multer.MulterError) {
-        console.log(err);
-        res
-          .status(500)
-          .send({
-            error: { msg: `multer uploading error: ${err.message}` },
-          })
-          .end();
-        return;
-      } else if (err) {
-        //unknown error
-        if (err.name == 'ExtensionError') {
-          res
-            .status(413)
-            .send({ error: { msg: `${err.message}` } })
-            .end();
-        } else {
-          res
-            .status(500)
-            .send({ error: { msg: `unknown uploading error: ${err.message}` } })
-            .end();
-        }
-        return;
-      }
-      res.status(200).send('file uploaded');
-    });
-});
+      cb(null, file.fieldname + '-' + Date.now() + file.originalname.match(/\..*$/)[0]);
+    }
+})
 
 const upload = multer({ storage: storage })
 
-router.post('/contact', upload.array('assets'), async (req, res) => {
+router.post('/contact', upload.array('assets', 10), async (req, res) => {
     try {
         //const validate = await contactDataSchema.validateAsync(req.body)
         const validate = req.body
